f24576200bbfacdc5baa7a9ee37582938057388d
onur.gokcen@halkbank.com.tr <onur.gokcen@halkbank.com.tr>
2022-09-09 00:15:53 +0300

    1. removeAll() ve foreachle arraylist

---
+++ HalkBankProjeOdevi/HalkBankCore/IBankList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalkBankCore
+{
+    public interface IBankList
+    {
+        void Ekle(object eleman);
+
+        void Sil(int index);
+
+        void KosulluSil(Func<object, bool> kosul);
+
+        void Temizle();
+
+        int ElemanSayisi { get; }
+
+        object this[int index]
+        {
+            get;
+            set;
+        }
+    }
+}
+
+//silme işlemi belirli bi koşul göre yapılmalı, arraylistte yok listte var, listteki removeall
---
+++ HalkBankProjeOdevi/HalkBankListDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using HalkBankCore;
+
+namespace HalkBankListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new HalkBankList();
+            list.Ekle("Salih");
+            list.Ekle("Engin");
+            list.Ekle("Mustafa");
+            list.Ekle("Ahmet");
+            //list.Ekle("Erol");
+            list.Sil(3);
+
+            list[2] = "Mehmet";
+            foreach (var eleman in list)
+            {
+                Console.WriteLine(eleman);
+            }
+            
+            Console.WriteLine("Eleman Sayısı:{0}", list.ElemanSayisi);
+            list.KosulluSil(list => list.Equals(1));
+            //Console.WriteLine("Eleman Sayısı:{0}", list.ElemanSayisi);
+
+            //for (int i = 0; i < list.ElemanSayisi; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+
+            foreach (var eleman in list)
+            {
+                Console.WriteLine(eleman);
+            }
+
+            //list.Temizle();
+
+            Console.WriteLine("Eleman Sayısı:{0}", list.ElemanSayisi);
+
+            Console.ReadLine();
+        }
+    }
+}
